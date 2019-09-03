33dcc48f71fe9966b6e0224cd82b9a72241fde6b
arifikhsan <arif.ikhsanudin.developer@gmail.com>
2019-09-03 14:50:04 +0700

    add custom backtrace

--- app/api/converter/custom_formatter.rb
+++ app/api/converter/custom_formatter.rb
@@ -1,5 +1,5 @@
 module Converter::CustomFormatter
   def self.call(message, backtrace, options, env, original_exception)
-    { message: message, options: options, backtrace: backtrace }.to_json
+    { success: false, code: options[:default_status], message: message, backtrace: backtrace[0..1] }.to_json
   end
 end
